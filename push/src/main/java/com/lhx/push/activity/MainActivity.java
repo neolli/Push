b79b26aee32683c10ad9acf37aa57f876be086fc
lihongxun <neo_lhx@163.com>
2016-04-06 22:34:03 +0800

    Initial commit

---
+++ push/src/main/java/com/lhx/push/activity/MainActivity.java
@@ -0,0 +1,47 @@
+package com.lhx.push.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.lhx.push.R;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
+
+/**
+ * 极光推送
+ */
+public class MainActivity extends Activity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+        // 设置别名
+        JPushInterface.setAlias(this, "test123456", new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+                Log.i(TAG, "set alias result is" + i);
+            }
+        });
+        // 设置Tag可以分组
+        Set<String> sets = new HashSet<String>();
+        sets.add("sports");
+        sets.add("game");
+        JPushInterface.setTags(this, sets, new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+                Log.i(TAG, "set tag result is" + i);
+            }
+        });
+    }
+}
